56f5c776334fcc12348b9c2b27a32379825a8fcc
Aeonitis <Aeonitis@users.noreply.github.com>
2021-05-16 18:53:23 +0100

    Go - Version compare via semver library

---
+++ version/VersionCompare.go
@@ -0,0 +1,63 @@
+package main
+
+/**
+Pre-requisite: go mod download github.com/blang/semver/v4
+*/
+import (
+	"log"
+	"os"
+	"github.com/blang/semver/v4"
+)
+
+/**
+Compare two version numbers version1 and version2.
+
+You may assume that the version strings are non-empty and contain only digits and the 'dot'
+character. The 'dot' character does not represent a decimal point and is used to separate
+number sequences. For instance '2.5' is not "two and a half" or "half way to version three", it is
+the fifth second-level revision of the second first-level revision.
+Here is an example of version numbers ordering: 0.1 < 1.1 < 1.2 < 1.2.9.9.9.9 < 1.3 < 1.3.4 < 1.10
+
+*/
+
+func main() {
+
+	// Take first two arguments
+	versionA := os.Args[1]
+	versionB := os.Args[2]
+
+	compareSemVer(versionA, versionB)
+}
+
+/**
+* Compare my way
+* Usage: go run VersionCompare.go 1.2 1.2.9.9.9.9
+ */
+func compareVersions(versionA, versionB string) {
+}
+
+/**
+* Via SemVer library For semantic versions, e.g. 1.0.0-beta with Major.Minor.Patch
+* Usage: go run VersionCompare.go 1.2.0 1.0.0-beta
+ */
+func compareSemVer(versionX, versionY string) {
+
+	log.Println("VersionX: " + versionX)
+	log.Println("VersionY: " + versionY)
+
+	validV1, err := semver.Make(versionX)
+	if err != nil {
+		log.Println("Error parsing version:", err)
+		return
+	}
+
+	validV2, err := semver.Make(versionY)
+	if err != nil {
+		log.Println("Error parsing version:", err)
+		return
+	}
+
+	log.Println("Result of version comaprison: ", validV1.Compare(validV2))
+	log.Println("Is v1 less than v2", validV1.LT(validV2))
+	log.Println("Is v1 greater than v2", validV1.GT(validV2))
+}
